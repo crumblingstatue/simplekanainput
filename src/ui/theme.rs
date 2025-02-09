96ebb533e96aa7014a0c1fe705f770d5ea7f009e
crumblingstatue <radiantstatue@gmail.com>
2025-02-09 09:51:35 +0100

    Update egui related dependencies

--- src/ui/theme.rs
+++ src/ui/theme.rs
@@ -6,22 +6,25 @@ use {
 };
 
 pub fn theme_ui(ui: &mut egui::Ui, app: &mut AppState) {
-    if ui.link("Back").clicked() {
+    if ui.link("Back").clicked() || ui.input(|inp| inp.key_pressed(egui::Key::Escape)) {
         app.ui_state = UiState::Input;
     }
     let colorix = app
         .colorix
-        .get_or_insert_with(|| Colorix::init(ui.ctx(), [ThemeColor::Custom([0, 0, 0]); 12]));
+        .get_or_insert_with(|| Colorix::global(ui.ctx(), [ThemeColor::Custom([0, 0, 0]); 12]));
     ui.group(|ui| {
         colorix.ui_combo_12(ui, true);
     });
-    colorix.themes_dropdown(ui, None, false);
-    colorix.light_dark_toggle_button(ui);
-    if ui.button("Randomize").clicked() {
-        let mut rng = rand::thread_rng();
-        *colorix = Colorix::init(
-            ui.ctx(),
-            std::array::from_fn(|_| ThemeColor::Custom(std::array::from_fn(|_| rng.r#gen()))),
-        );
-    }
+    ui.horizontal(|ui| {
+        colorix.themes_dropdown(ui, None, false);
+        ui.label("Light/dark");
+        colorix.light_dark_toggle_button(ui, 20.0);
+        if ui.button("Randomize").clicked() {
+            let mut rng = rand::thread_rng();
+            *colorix = Colorix::global(
+                ui.ctx(),
+                std::array::from_fn(|_| ThemeColor::Custom(std::array::from_fn(|_| rng.r#gen()))),
+            );
+        }
+    });
 }
